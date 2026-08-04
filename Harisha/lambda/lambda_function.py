0dbda0363eb69fb674e91ff2a61e7e0fc3934fd3
Hari Udayakumar <your_email@example.com>
2026-08-04 18:43:04 +1000

    Update Lambda website monitoring function

--- Harisha/lambda/lambda_function.py
+++ Harisha/lambda/lambda_function.py
@@ -1,5 +1,28 @@
+import urllib.request
+import time
+
+
 def lambda_handler(event, context):
-    return {
-        "statusCode":200,
-        "body": "Hello World from Lambda!"
-    }
+    
+    url = "http://www.westernsydney.edu.au/"
+
+    start_time = time.time()
+
+    try:
+        response = urllib.request.urlopen(url)
+
+        end_time = time.time()
+
+        return{
+            "website":url,
+            "status_code": response.status,
+            "response_time": round(end_time - start_time, 3),
+            "status": "UP"
+        }
+
+    except Exception as e:
+      return {
+        "website": url,
+        "status": "DOWN",
+        "error": str(e)
+      }
